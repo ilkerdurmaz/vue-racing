441c512f66a6d0f2c7f00d2846920b857b168caa
ilkerdurmaz <ilker.prime@gmail.com>
2022-10-16 06:22:46 +0300

    add PingText component
    animated component for countdown etc.

---
+++ src/assets/components/PingText.vue
@@ -0,0 +1,29 @@
+<script setup>
+const props = defineProps({
+  text: {
+    type: String,
+    default: "Ping Text",
+  },
+  size: {
+    type: String,
+    default: "100",
+  },
+  color: {
+    type: String,
+    default: "black",
+  },
+  ping: {
+    type: Boolean,
+    default: true,
+  },
+});
+</script>
+
+<template>
+  <div
+    :class="{ 'animate-ping': props.ping }"
+    :style="{ fontSize: `${props.size}px`, color: props.color }"
+  >
+    {{ props.text }}
+  </div>
+</template>
