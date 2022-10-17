c8666e203ed72bdef82d3c0f433aed02fb3fe46a
ilkerdurmaz <ilker.prime@gmail.com>
2022-10-17 03:15:10 +0300

    Add Line Component
    created customizable line component for start and finish line

---
+++ src/components/Lane/Line.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps({
+  background: {
+    type: String,
+    default: "red",
+  },
+  width: {
+    type: Number,
+    default: 10,
+  },
+  backgroundSize: {
+    type: Number,
+    default: 100,
+  },
+});
+</script>
+
+<template>
+  <div
+    :style="{
+      background,
+      width: width + 'px',
+      backgroundSize: backgroundSize + 'px',
+    }"
+  ></div>
+</template>
