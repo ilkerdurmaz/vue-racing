e49b778bcc72233b74c8350cfc35adffa1f92948
ilkerdurmaz <ilker.prime@gmail.com>
2022-10-23 23:37:17 +0300

    Update button components
    changed folder structer and some classes for better usage

---
+++ src/components/Shared/IconButton.vue
@@ -0,0 +1,12 @@
+<script setup>
+const emit = defineEmits(["click"]);
+</script>
+
+<template>
+  <button
+    @click="emit('click')"
+    class="rounded-lg px-2 py-1 inline-flex items-center justify-center focus:ring-4 focus:ring-pink-200 dark:focus:ring-pink-800 transition-all"
+  >
+    <slot></slot>
+  </button>
+</template>
