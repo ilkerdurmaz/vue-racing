851ac62ca73cd6342261e10fbe5f27a6d7464806
ilkerdurmaz <ilker.prime@gmail.com>
2022-10-25 12:28:37 +0300

    Add Settings Components
    SettingsComp and LaneSettingsComp added for configuring lane colors and racer names

---
+++ src/components/SettingsComp/LaneSettingsComp.vue
@@ -0,0 +1,68 @@
+<script setup>
+import { reactive } from "vue";
+
+const props = defineProps({
+  laneSettings: {
+    type: Object,
+    default: {},
+  },
+  laneIndex: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const emit = defineEmits(["settingsChanged"]);
+const changedLaneSettings = reactive({
+  laneColor: props.laneSettings.laneColor,
+  racerName: props.laneSettings.racerName,
+});
+
+function nameChangeHandler(e) {
+  changedLaneSettings.racerName = e.target.value;
+  emit("settingsChanged", changedLaneSettings);
+}
+function laneColorChangeHandler(e) {
+  changedLaneSettings.laneColor = e.target.value;
+  emit("settingsChanged", changedLaneSettings);
+}
+</script>
+
+<template>
+  <div
+    class="flex gap-2 dark:text-white border rounded-md p-2 border-neutral-400"
+  >
+    <span
+      class="flex h-8 w-8 items-center justify-center rounded-full text-white bg-gradient-to-br from-pink-500 to-orange-400 pb-0.5 text-lg"
+      >{{ laneIndex + 1 }}</span
+    >
+    <input
+      type="text"
+      :value="props.laneSettings.racerName"
+      @change="nameChangeHandler"
+      placeholder="Racer Name"
+      class="border rounded px-1 bg-transparent border-neutral-400 dark:border-neutral-700 w-40"
+    />
+    <span class="flex items-center gap-2 text-xs sm:text-base"
+      >Lane Color:
+      <input
+        type="color"
+        :value="props.laneSettings.laneColor"
+        @change="laneColorChangeHandler"
+        class="color-picker"
+    /></span>
+  </div>
+</template>
+
+<style scoped>
+.color-picker {
+  -webkit-appearance: none;
+  @apply p-0 border border-neutral-700 rounded-full w-8 h-8;
+}
+.color-picker::-webkit-color-swatch {
+  @apply p-0 border-none rounded-full;
+}
+.color-picker::-webkit-color-swatch-wrapper {
+  @apply p-0 border-none rounded-full;
+}
+</style>
---
+++ src/components/SettingsComp/SettingsComp.vue
@@ -0,0 +1,25 @@
+<script setup>
+import LaneSettings from "./LaneSettingsComp.vue";
+const props = defineProps(["lanes"]);
+const emit = defineEmits(["update:lanes"]);
+
+function settingsChangedHandler(val, index) {
+  const updatedSettings = [...props.lanes];
+  updatedSettings[index] = val;
+  emit("update:lanes", updatedSettings);
+}
+</script>
+
+<template>
+  <div class="flex flex-col gap-2 p-2 sm:p-3">
+    <LaneSettings
+      v-for="(lane, index) in lanes"
+      :key="lane.racerName"
+      :lane-settings="lane"
+      :lane-index="index"
+      @settings-changed="(val) => settingsChangedHandler(val, index)"
+    ></LaneSettings>
+  </div>
+</template>
+
+<style scoped></style>
