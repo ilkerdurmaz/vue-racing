21bdfb02f2f0fd1bb801de70c1236a422010b787
ilkerdurmaz <ilker.prime@gmail.com>
2022-10-25 12:26:51 +0300

    Update GradientButton Component
    props syntax improved and "btnDisable" prop added

--- src/components/Shared/GradientButton.vue
+++ src/components/Shared/GradientButton.vue
@@ -1,8 +1,14 @@
 <script setup>
-defineProps({
-  text: {
+import { computed } from "vue";
+
+const props = defineProps({
+  btnText: {
     type: String,
     default: "Button",
+  },
+  btnDisable: {
+    type: Boolean,
+    default: false,
   },
 });
 
@@ -12,12 +18,15 @@ const emit = defineEmits(["click"]);
 <template>
   <button
     @click="emit('click')"
+    :disabled="props.btnDisable"
     class="inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800"
+    :class="{ 'opacity-40': props.btnDisable }"
   >
     <span
-      class="px-5 py-2.5 transition-all ease-in duration-75 bg-zinc-100 dark:bg-zinc-800 rounded-md group-hover:bg-opacity-0"
+      class="px-5 py-2.5 transition-all ease-in duration-75 bg-zinc-100 dark:bg-zinc-800 rounded-md"
+      :class="{ 'group-hover:bg-opacity-0': !props.btnDisable }"
     >
-      {{ text }}
+      {{ props.btnText }}
     </span>
   </button>
 </template>
