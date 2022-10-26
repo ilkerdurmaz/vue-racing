87502f3b36588e8435d6875a3252cc52e3d55927
ilkerdurmaz <ilker.prime@gmail.com>
2022-10-26 12:00:35 +0300

    Update App Component
    configured for sorting lanes and adding finished state checker algorithms

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from "vue";
+import { ref, computed, watch, onUpdated } from "vue";
 import Lane from "./components/LaneComp/LaneComp.vue";
 import List from "./components/ListComp/ListComp.vue";
 import HeaderComp from "./components/HeaderComp.vue";
@@ -8,6 +8,7 @@ import FooterComp from "./components/FooterComp.vue";
 import SettingsComp from "./components/SettingsComp/SettingsComp.vue";
 import laneData from "./assets/data/lanes.json";
 import PingText from "./components/PingTextComp.vue";
+import GradientButton from "./components/Shared/GradientButton.vue";
 
 const darkMode = ref(
   localStorage.getItem("darkMode")
@@ -21,16 +22,22 @@ const lanes = ref(
 );
 const isStarted = ref(false);
 const showModal = ref(false);
-const startButtonDisabled = ref(false);
 const count = ref(5);
 const countDownShow = ref(false);
+const tempList = ref([...lanes.value]);
+
+const sortedLanes = computed(() => {
+  return tempList.value.sort((a, b) => b.position - a.position);
+});
+
+const isFinished = computed(() => {
+  return sortedLanes.value[sortedLanes.value.length - 1].position == 100;
+});
 
 function darkModeHandler() {
   darkMode.value = !darkMode.value;
   localStorage.setItem("darkMode", darkMode.value);
 }
-
-function livePositions(racerInfo) {}
 
 function saveSettings() {
   showModal.value = false;
@@ -39,15 +46,21 @@ function saveSettings() {
 
 function closeWithoutSave() {
   showModal.value = false;
-  lanes.value = laneData;
+  // lanes.value = localStorage.getItem("laneSettings")
+  //   ? JSON.parse(localStorage.getItem("laneSettings"))
+  //   : laneData;
+}
+
+function resetLanes() {
+  isStarted.value = false;
 }
 
 function startRace() {
+  resetLanes();
   countDownToStart();
 }
 
 function countDownToStart() {
-  startButtonDisabled.value = true;
   countDownShow.value = true;
   const interval = setInterval(() => {
     count.value--;
@@ -55,16 +68,18 @@ function countDownToStart() {
       clearInterval(interval);
       isStarted.value = true;
       countDownShow.value = false;
+      count.value = 5;
     }
   }, 1000);
 }
 </script>
+
 <template>
   <div :class="{ dark: darkMode }">
     <main class="min-h-screen dark:bg-neutral-900">
       <HeaderComp
         title="Vue Racing App"
-        :main-btn-disable="startButtonDisabled"
+        :main-btn-disable="(isStarted && !isFinished) || countDownShow"
         main-btn-text="Start Race"
         :dark-mode="darkMode"
         @mainBtnClick="startRace"
@@ -73,7 +88,7 @@ function countDownToStart() {
       <div
         class="grid grid-cols-4 rounded-lg max-w-7xl mx-auto gap-2 px-1 mt-2 sm:mt-4 sm:gap-4 sm:px-4"
       >
-        <div class="col-span-4 lg:col-span-3">
+        <div class="col-span-4 xl:col-span-3">
           <div class="overflow-hidden rounded-lg relative">
             <PingText
               color="white"
@@ -84,23 +99,14 @@ function countDownToStart() {
               v-for="(lane, index) in lanes"
               :key="lane.racerName"
               :background="lane.laneColor"
-              lineBg="black"
+              lineBg="url('https://d2gg9evh47fn9z.cloudfront.net/1600px_COLOURBOX2446338.jpg')"
               :started="isStarted"
-              @position="
-                (val) =>
-                  livePositions({
-                    position: val.position,
-                    speed: val.speed,
-                    laneNumber: index,
-                    laneColor: lane.laneColor,
-                    racerName: lane.racerName,
-                  })
-              "
+              @img-position-changed="(val) => (lanes[index].position = val)"
             ></Lane>
           </div>
         </div>
-        <div class="col-span-4 lg:col-span-1">
-          <List></List>
+        <div class="col-span-4 xl:col-span-1">
+          <List :list="sortedLanes"></List>
         </div>
       </div>
 
