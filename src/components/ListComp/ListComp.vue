5b7441f9e284e8456c5f99f953f998bbb3a23b2f
ilkerdurmaz <ilker.prime@gmail.com>
2022-10-26 11:57:56 +0300

    Update List and ListItem Component
    component names changed and configured for responsivity

---
+++ src/components/ListComp/ListComp.vue
@@ -0,0 +1,64 @@
+<script setup>
+import ListItem from "./ListItemComp.vue";
+const props = defineProps({
+  list: {
+    type: Array,
+    default: [],
+  },
+});
+</script>
+
+<template>
+  <div class="list">
+    <div class="list-background">
+      <div class="list-header">
+        <h1 class="title">Leaderboard</h1>
+      </div>
+      <div class="list-body">
+        <div
+          class="flex text-xs text-neutral-400 divide-x dark:divide-neutral-700"
+        >
+          <span class="p-1 pl-0">PLACE</span>
+          <span class="p-1">RACER NAME</span>
+          <span class="ml-auto p-1">LANE</span>
+        </div>
+
+        <ListItem
+          v-for="(lane, index) in props.list"
+          :key="lane.laneNumber"
+          :index="index + 1"
+        >
+          <div class="text-lg flex items-center justify-between gap-2 w-full">
+            <span>{{ lane.racerName }}</span>
+            <span
+              class="flex w-8 h-8 shrink-0 items-center justify-center rounded-full"
+              ><i
+                class="bi text-2xl px-2 flex"
+                :class="[`bi-${lane.laneNumber}-circle`]"
+                :style="{ color: lane.laneColor }"
+              ></i
+            ></span>
+          </div>
+        </ListItem>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.list {
+  @apply overflow-hidden rounded-lg shadow;
+}
+.list-background {
+  @apply p-0.5  bg-gradient-to-br from-pink-500 to-orange-400 dark:from-neutral-800 dark:to-neutral-800;
+}
+.list-header {
+  @apply flex items-center gap-3 p-2 sm:p-3;
+}
+.title {
+  @apply text-xl sm:text-2xl text-white;
+}
+.list-body {
+  @apply flex flex-col divide-y px-2 dark:divide-neutral-700 dark:text-white bg-white dark:bg-neutral-900 rounded-md;
+}
+</style>
---
+++ src/components/ListComp/ListItemComp.vue
@@ -0,0 +1,20 @@
+<script setup>
+const props = defineProps({
+  index: {
+    type: Number,
+    default: 0,
+  },
+});
+</script>
+
+<template>
+  <div class="flex items-center gap-2 py-1 lg:py-2.5">
+    <span
+      class="flex shrink-0 h-8 w-8 items-center justify-center rounded-full text-white bg-gradient-to-br from-pink-500 to-orange-400 pb-0.5 text-lg"
+      >{{ props.index }}.</span
+    >
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped></style>
