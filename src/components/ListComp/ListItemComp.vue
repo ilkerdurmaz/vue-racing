1760d0afef4456922312fd457d886c5412a2c18c
ilkerdurmaz <ilker.prime@gmail.com>
2022-10-28 07:15:24 +0300

    Update ListItem component
    lane number symbol changed to standart css styling from bootstrap icon

--- src/components/ListComp/ListItemComp.vue
+++ src/components/ListComp/ListItemComp.vue
@@ -10,12 +10,8 @@ const props = defineProps({
 <template>
   <div class="flex items-center gap-2 py-1 lg:py-2.5 xl:py-4">
     <span
-      class="flex shrink-0 h-8 w-8 items-center justify-center rounded-full text-white text-lg border-2 serif-font"
-      :class="[
-        index > 3
-          ? 'gradient-others border-none'
-          : `gradient-${props.index}  text-white`,
-      ]"
+      class="index-symbol font-serif"
+      :class="[index > 3 ? 'gradient-others' : `gradient-${props.index}`]"
       >{{ props.index }}</span
     >
     <slot></slot>
@@ -23,19 +19,19 @@ const props = defineProps({
 </template>
 
 <style scoped>
+.index-symbol {
+  @apply flex shrink-0 h-8 w-8 items-center justify-center rounded-full text-white border-2 text-lg text-shadow-sm shadow-black;
+}
 .gradient-others {
-  @apply bg-gradient-to-br from-pink-500 to-orange-400;
+  @apply bg-gradient-to-br from-pink-500 to-orange-400 dark:border-neutral-700;
 }
 .gradient-1 {
   @apply bg-gradient-to-br from-[#BF953F] via-[#FBF5B7] to-[#BF953F] border-[#BF953F];
-  text-shadow: 0 0 3px #bf953f;
 }
 .gradient-2 {
   @apply bg-gradient-to-br from-[#878684] via-[#E3E3E3] to-[#878684] border-[#878684];
-  text-shadow: 0 0 3px #878684;
 }
 .gradient-3 {
   @apply bg-gradient-to-br from-[#6E3B07] via-[#B28E58] to-[#6E3B07] border-[#6E3B07];
-  text-shadow: 0 0 3px #6e3b07;
 }
 </style>
