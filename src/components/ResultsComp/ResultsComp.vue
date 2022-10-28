94888199c95174363c728976a61061ae2cdc3eb8
ilkerdurmaz <ilker.prime@gmail.com>
2022-10-28 07:12:26 +0300

    Update Results component
    font-shadow classes updated

--- src/components/ResultsComp/ResultsComp.vue
+++ src/components/ResultsComp/ResultsComp.vue
@@ -30,15 +30,15 @@ const emit = defineEmits(["closeBtnClick"]);
           <div class="bar-text">
             {{ props.results[1].racerName }}
           </div>
-          <div class="silver serif-font">2</div>
+          <div class="silver font-serif">2</div>
         </div>
         <div class="podium-bar">
           <div class="bar-text">{{ props.results[0].racerName }}</div>
-          <div class="gold serif-font">1</div>
+          <div class="gold font-serif">1</div>
         </div>
         <div class="podium-bar">
           <div class="bar-text">{{ props.results[2].racerName }}</div>
-          <div class="bronze serif-font">3</div>
+          <div class="bronze font-serif">3</div>
         </div>
       </div>
       <span class="text-xs text-neutral-300 py-1"
@@ -65,16 +65,13 @@ const emit = defineEmits(["closeBtnClick"]);
   @apply mb-1 text-xs sm:text-base md:text-lg;
 }
 .gold {
-  @apply bg-gradient-to-r from-[#BF953F] via-[#FBF5B7] to-[#BF953F] text-9xl rounded-t-md h-36 sm:h-48;
-  text-shadow: 0 0 3px #bf953f;
+  @apply bg-gradient-to-r from-[#BF953F] via-[#FBF5B7] to-[#BF953F] text-9xl rounded-t-md h-36 sm:h-48 text-shadow shadow-[#BF953F];
 }
 .silver {
-  @apply bg-gradient-to-r from-[#878684] via-[#E3E3E3] to-[#878684] text-8xl rounded-l-md  h-24 sm:h-32;
-  text-shadow: 0 0 3px #878684;
+  @apply bg-gradient-to-r from-[#878684] via-[#E3E3E3] to-[#878684] text-8xl rounded-l-md  h-24 sm:h-32 text-shadow shadow-[#878684];
 }
 .bronze {
-  @apply bg-gradient-to-r from-[#6E3B07] via-[#B28E58] to-[#6E3B07] text-5xl md:text-6xl rounded-r-md h-12 sm:h-16;
-  text-shadow: 0 0 3px #6e3b07;
+  @apply bg-gradient-to-r from-[#6E3B07] via-[#B28E58] to-[#6E3B07] text-5xl md:text-6xl rounded-r-md h-12 sm:h-16 text-shadow shadow-[#6E3B07];
 }
 .backdrop-transition-enter-active,
 .backdrop-transition-leave-active {
