0e6fba959e2dde2413df2de18d979d1dbb835c93
ilkerdurmaz <ilker.prime@gmail.com>
2022-10-28 07:08:56 +0300

    Update tailwind config
    text-shadow class added and font-family for serif specified

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,33 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require("tailwindcss/plugin");
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      textShadow: {
+        xs: "0 0px 1px var(--tw-shadow-color)",
+        sm: "0 1px 2px var(--tw-shadow-color)",
+        DEFAULT: "0 2px 4px var(--tw-shadow-color)",
+        md: "0 2px 4px var(--tw-shadow-color)",
+        lg: "0 8px 16px var(--tw-shadow-color)",
+      },
+    },
+    fontFamily: {
+      serif: ["Times New Roman", "Times", "serif"],
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          "text-shadow": (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme("textShadow") }
+      );
+    }),
+  ],
   darkMode: "class",
 };
